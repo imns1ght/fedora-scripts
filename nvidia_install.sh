d19a94125a16a8fe86e299189964eb6b3ad1e34c
imns1ght <jeffersonbrunoit@gmail.com>
2024-09-10 14:05:24 -0300

    Add nvidia instructions to avoid v.555 stutterings

--- nvidia_install.sh
+++ nvidia_install.sh
@@ -7,13 +7,13 @@ update_output=$(sudo dnf update -y)
 echo ">>> Update completed"
 # Check if there were any updates applied
 if echo "$update_output" | grep -q 'Upgraded:'; then
-    echo ">>> Updates were installed. The system will restart now."
-    echo "Restarting in 10 seconds..."
-    sleep 10
-    # Restart the system
-    sudo shutdown -r now
+  echo ">>> Updates were installed. The system will restart now."
+  echo "Restarting in 10 seconds..."
+  sleep 10
+  # Restart the system
+  sudo shutdown -r now
 else
-    echo ">>> No updates were applied. No need to restart."
+  echo ">>> No updates were applied. No need to restart."
 fi
 
 echo ">>> Preparing to enable Secure Boot"
@@ -28,15 +28,17 @@ sleep 10
 sudo shutdown -r now
 
 echo ">>> Installing Nvidia drivers"
-sudo dnf install akmod-nvidia -y # Use kmod-nvidia for RHEL/CentOS
+sudo dnf install akmod-nvidia -y             # Use kmod-nvidia for RHEL/CentOS
 sudo dnf install xorg-x11-drv-nvidia-cuda -y # Optional for CUDA support
 # Check if the Nvidia module is loaded and has a version
-if modinfo -F version nvidia > /dev/null 2>&1; then
-    echo ">>> Nvidia driver installation successful. The system will restart now."
-    echo "Restarting in 10 seconds..."
-    sleep 10
-    # Restart the system
-    sudo shutdown -r now
+if modinfo -F version nvidia >/dev/null 2>&1; then
+  echo ">>> Nvidia driver installation successful. The system will restart now."
+  echo "Restarting in 10 seconds..."
+  sleep 10
+  # Restart the system
+  sudo shutdown -r now
 else
-    echo ">>> Nvidia driver installation failed or the driver is not loaded. Please check the installation logs."
+  echo ">>> Nvidia driver installation failed or the driver is not loaded. Please check the installation logs."
 fi
+
+## Should add nvidia.NVreg_EnableGpuFirmware=0 to improve 555 driver version performance.
